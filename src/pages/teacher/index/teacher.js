4b472b600a7a9aa3837d9bec5f8c190ade5710d8
king <zs17337825170@163.com>
2020-04-08 19:18:34 +0800

    教师页面完善路由，引入redux

--- src/pages/teacher/index/teacher.js
+++ src/pages/teacher/index/teacher.js
@@ -1,15 +1,22 @@
 import React from 'react'
-import { Layout, Menu, Breadcrumb } from 'antd';
+import {Route, Switch} from "react-router-dom";
+import {Layout, Menu, Breadcrumb} from 'antd';
 import {
     DesktopOutlined,
     PieChartOutlined,
-    FileOutlined,
+    CommentOutlined,
     TeamOutlined,
-    UserOutlined,
+    EditOutlined,
+    NotificationOutlined,
+    IdcardOutlined
 } from '@ant-design/icons';
 
-const { Header, Content, Footer, Sider } = Layout;
-const { SubMenu } = Menu;
+import './teacher.less'
+
+import StudentInform from "../studentInform/studentInform";
+
+const {Header, Content, Footer, Sider} = Layout;
+const {SubMenu} = Menu;
 
 export default class Teacher extends React.Component {
 
@@ -18,65 +25,63 @@ export default class Teacher extends React.Component {
     };
     onCollapse = collapsed => {
         console.log(collapsed);
-        this.setState({ collapsed });
+        this.setState({collapsed});
     };
+
     render() {
         return (
             <div>
-                <Layout style={{ minHeight: '100vh' }}>
+                <Layout style={{minHeight: '100vh'}}>
                     <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse}>
-                        <div className="logo" />
+                        <div className="myLogo"/>
                         <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
                             <Menu.Item key="1">
-                                <PieChartOutlined />
-                                <span>Option 1</span>
+                                <TeamOutlined/>
+                                <span>学生信息</span>
                             </Menu.Item>
                             <Menu.Item key="2">
-                                <DesktopOutlined />
-                                <span>Option 2</span>
+                                <DesktopOutlined/>
+                                <span>上课记录</span>
                             </Menu.Item>
-                            <SubMenu
-                                key="sub1"
-                                title={
-                                    <span>
-                  <UserOutlined />
-                  <span>User</span>
-                </span>
-                                }
-                            >
-                                <Menu.Item key="3">Tom</Menu.Item>
-                                <Menu.Item key="4">Bill</Menu.Item>
-                                <Menu.Item key="5">Alex</Menu.Item>
-                            </SubMenu>
-                            <SubMenu
-                                key="sub2"
-                                title={
-                                    <span>
-                  <TeamOutlined />
-                  <span>Team</span>
-                </span>
-                                }
-                            >
-                                <Menu.Item key="6">Team 1</Menu.Item>
-                                <Menu.Item key="8">Team 2</Menu.Item>
-                            </SubMenu>
-                            <Menu.Item key="9">
-                                <FileOutlined />
+                            <Menu.Item key="3">
+                                <CommentOutlined />
+                                <span>课下反馈</span>
+                            </Menu.Item>
+                            <Menu.Item key="4">
+                                <PieChartOutlined />
+                                <span>课程统计</span>
+                            </Menu.Item>
+                            <Menu.Item key="5">
+                                <EditOutlined />
+                                <span>课堂评价</span>
+                            </Menu.Item>
+                            <Menu.Item key="6">
+                                <NotificationOutlined />
+                                <span>消息通知</span>
+                            </Menu.Item>
+                            <Menu.Item key="7">
+                                <IdcardOutlined />
+                                <span>个人信息</span>
                             </Menu.Item>
                         </Menu>
                     </Sider>
                     <Layout className="site-layout">
-                        <Header className="site-layout-background" style={{ padding: 0 }} />
-                        <Content style={{ margin: '0 16px' }}>
-                            <Breadcrumb style={{ margin: '16px 0' }}>
+                        <Header className="site-layout-background" style={{padding: 0}}>
+                            <h1 style={{paddingLeft:25}}>教师后台管理</h1>
+                        </Header>
+                        <Content style={{margin: '0 16px'}}>
+                            <Breadcrumb style={{margin: '16px 0'}}>
                                 <Breadcrumb.Item>User</Breadcrumb.Item>
                                 <Breadcrumb.Item>Bill</Breadcrumb.Item>
                             </Breadcrumb>
-                            <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+                            <div className="site-layout-background" style={{padding: 24, minHeight: 360}}>
+                                <Switch>
+                                    <Route path='/teacherPage/studentInform' component={StudentInform} />
+                                </Switch>
                                 Bill is a cat.
                             </div>
                         </Content>
-                        <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+                        <Footer style={{textAlign: 'center'}}>Music Learning ©2020 Created by Zhao Sheng</Footer>
                     </Layout>
                 </Layout>
             </div>
---
+++ src/pages/teacher/studentInform/studentInform.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+export default class StudentInform extends React.Component {
+
+    state = {};
+
+    render() {
+        return (
+            <div>
+                StudentInform
+            </div>
+        )
+    }
+}
