244abc7b69acbc3ba770ee97df2fcd7dc85bccb8
king <zs17337825170@163.com>
2020-04-10 17:05:17 +0800

    新增学生信息数据页面

--- src/pages/teacher/studentInform/studentInform.js
+++ src/pages/teacher/studentInform/studentInform.js
@@ -1,15 +1,46 @@
 import React from 'react'
-import {Breadcrumb} from "antd";
+import {Card, Col, Row,Avatar, Statistic} from "antd";
 
+const { Meta } = Card;
 
 export default class StudentInform extends React.Component {
 
-    state = {};
+    state = {
+        student:[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+    };
 
     render() {
         return (
             <div>
-                StudentInform
+                <Row gutter={[24,16]}>
+                    {
+                        this.state.student.map((name,index)=>{
+                            return(
+                                <Col span={6} key={index}>
+                                    <Card
+                                        hoverable
+                                    >
+                                        <Meta
+                                            avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+                                            title={`学生${name}`}
+                                            description={
+                                                <Row gutter={16}>
+                                                    <Col span={12}>
+                                                        <Statistic title="上课次数" value={name} />
+                                                    </Col>
+                                                    <Col span={12}>
+                                                        <Statistic title="出勤率" value={95} suffix="%"  />
+                                                    </Col>
+                                                </Row>
+                                            }
+                                        />
+                                    </Card>
+                                </Col>
+                            )
+                        })
+                    }
+                 ]
+                </Row>
             </div>
         )
     }
