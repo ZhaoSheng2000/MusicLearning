9a65a9f822acec57c6538439277a838c81da93a2
king <zs17337825170@163.com>
2020-04-11 15:34:26 +0800

    完成上课记录页面

--- src/pages/teacher/classRecord/classRecord.js
+++ src/pages/teacher/classRecord/classRecord.js
@@ -1,6 +1,21 @@
 import React from 'react'
+import { Statistic, Row, Col, Card, List } from 'antd';
 
 
+const data = [
+    {
+        title: 'Ant Design Title 1',
+    },
+    {
+        title: 'Ant Design Title 2',
+    },
+    {
+        title: 'Ant Design Title 3',
+    },
+    {
+        title: 'Ant Design Title 4',
+    },
+];
 export default class ClassRecord extends React.Component {
 
     state = {};
@@ -8,7 +23,39 @@ export default class ClassRecord extends React.Component {
     render() {
         return (
             <div>
-                ClassRecord
+                <Card>
+                    <Row gutter={16}>
+                        <Col span={8}>
+                            <Statistic title="我的课程" value={8} suffix={' 个'} />
+                        </Col>
+                        <Col span={8}>
+                            <Statistic title="待处理课程" value={3} suffix={'个'} />
+                        </Col>
+                        <Col span={8}>
+                            <Statistic title="本周已上课程" value={3} suffix={'节'} />
+                        </Col>
+                    </Row>
+                </Card>
+                <br/>
+                <br/>
+                <Card>
+                    <List
+                        header={<h1>我的课程</h1>}
+                        size={'large'}
+                        dataSource={data}
+                        renderItem={item => (
+                            <List.Item
+                                actions={[<a key="list-loadmore-edit">编辑上课记录</a>]}
+                            >
+                                <List.Item.Meta
+                                    title={<a href={'/#/teacherPage'}>{item.title}</a>}
+                                    description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+                                />
+                            </List.Item>
+                        )}
+                    />
+                </Card>
+
             </div>
         )
     }
