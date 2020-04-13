051078a95d2bc17d97fdf6ad5e92fc584fbb2097
king <zs17337825170@163.com>
2020-04-13 11:17:54 +0800

    添加管理员页面

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import {HashRouter, Route, Switch} from "react-router-dom";
 
-import Student from './pages/student/index/student'
+import Student from './pages/student/index/student';
 import Teacher from "./pages/teacher/index/teacher";
+import Admin from "./pages/admin/index/admin";
 
 function App() {
     return (
         <HashRouter>
             <Switch>
                 <Route path='/teacherPage' component={Teacher}/>
+                <Route path={'/admin'} component={Admin}/>
                 <Route path='/' component={Student}/>
             </Switch>
         </HashRouter>
