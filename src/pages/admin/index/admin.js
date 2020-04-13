a1638113fb9d68895c40888b97744037d0d1b3e8
king <zs17337825170@163.com>
2020-04-13 15:21:59 +0800

    添加编辑学生信息

--- src/pages/admin/index/admin.js
+++ src/pages/admin/index/admin.js
@@ -1,18 +1,17 @@
 import React from 'react'
+import {Route, Switch} from "react-router-dom";
 import { Layout, Menu } from 'antd';
 
 import {
-    AppstoreOutlined,
     BarChartOutlined,
     CloudOutlined,
-    ShopOutlined,
-    TeamOutlined,
     UserOutlined,
     UploadOutlined,
-    VideoCameraOutlined,
 } from '@ant-design/icons';
 
 import './admin.less'
+
+import StudentInform from "../studentInform/studentInform";
 
 const { Header, Content, Footer, Sider } = Layout;
 
@@ -36,45 +35,34 @@ export default class Admin extends React.Component {
                         <div className="myLogo" />
                         <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
                             <Menu.Item key="1">
-                                <UserOutlined />
-                                <span className="nav-text">nav 1</span>
-                            </Menu.Item>
-                            <Menu.Item key="2">
-                                <VideoCameraOutlined />
-                                <span className="nav-text">nav 2</span>
+                                <a href={'/#/admin'}>
+                                    <UserOutlined />
+                                    <span className="nav-text">编辑学生信息</span>
+                                </a>
                             </Menu.Item>
                             <Menu.Item key="3">
                                 <UploadOutlined />
-                                <span className="nav-text">nav 3</span>
-                            </Menu.Item>
-                            <Menu.Item key="4">
-                                <BarChartOutlined />
-                                <span className="nav-text">nav 4</span>
+                                <span className="nav-text">上传教师信息</span>
                             </Menu.Item>
                             <Menu.Item key="5">
                                 <CloudOutlined />
-                                <span className="nav-text">nav 5</span>
+                                <span className="nav-text">更新实时资讯</span>
                             </Menu.Item>
                             <Menu.Item key="6">
-                                <AppstoreOutlined />
-                                <span className="nav-text">nav 6</span>
-                            </Menu.Item>
-                            <Menu.Item key="7">
-                                <TeamOutlined />
-                                <span className="nav-text">nav 7</span>
-                            </Menu.Item>
-                            <Menu.Item key="8">
-                                <ShopOutlined />
-                                <span className="nav-text">nav 8</span>
+                                <BarChartOutlined />
+                                <span className="nav-text">出勤率图表</span>
                             </Menu.Item>
                         </Menu>
                     </Sider>
                     <Layout className="site-layout" style={{ marginLeft: 200 }}>
-                        <Header className="site-layout-background" style={{ padding: 0 }} />
-                        <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
-                            <div className="site-layout-background" style={{ padding: 24, textAlign: 'center',minHeight:window.innerHeight-100 }}>
-                                content
-                            </div>
+                        <Header className="site-layout-background" style={{padding: 0}}>
+                            <h1 style={{paddingLeft: 25}}>后台管理</h1>
+                        </Header>
+                        <Content style={{margin: '16px 16px'}}>
+                            <Switch>
+
+                                <Route  component={StudentInform}/>
+                            </Switch>
                         </Content>
                         <Footer style={{textAlign: 'center'}}>Music Learning ©2020 Created by Zhao Sheng</Footer>
                     </Layout>
---
+++ src/pages/admin/studentInform/studentInform.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {Card} from "antd";
+
+
+export default class StudentInform extends React.Component {
+
+    state = {};
+
+    render() {
+        return (
+            <div>
+                <Card>
+                    studentinform
+                </Card>
+            </div>
+        )
+    }
+}
